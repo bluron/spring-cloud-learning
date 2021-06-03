5a76bbd89dd63ebec5aa3311c2819d38eda76097
bluron <julei_me@163.com>
2021-06-03 17:22:18 +0800

    eureka服务注册中心

---
+++ eureka-server/src/test/java/org/bluron/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bluron.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
